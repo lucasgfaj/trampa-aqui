1c499f1cf0eb801fe3f453bebb301cd47f5cdba5
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-02 15:58:14 -0300

    Add: Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimeiroComponenteComponent, SegundoComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
