b9fb224f77fe3000deadbf8d6be131fd5fdae02b
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-07 19:41:28 -0300

    End: Footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
