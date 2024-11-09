8c0969591207f5872c23fd4e2a7f4dbd61df98ba
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-09 16:14:11 -0300

    Update: Dirs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
