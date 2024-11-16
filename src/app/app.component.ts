3f631c428467d2f394a3a7860f735cda08261bdc
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-16 11:17:31 -0300

    Edit: Componnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from "./pages/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
