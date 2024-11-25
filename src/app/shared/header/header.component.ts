7a33b83d2d424137b1d6cef51b2ef6779b619e20
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-24 22:21:30 -0300

    Edit: Header - Home

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,12 +5,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
-
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, MatListModule, MatSidenavModule, CommonModule],
+  imports: [MatMenuModule, MatToolbarModule, MatButtonModule, RouterLink, MatListModule, MatSidenavModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
