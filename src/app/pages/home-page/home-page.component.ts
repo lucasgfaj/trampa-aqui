800c4eb5670ca00d7fd8de1eb8c8120369d7bb56
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-29 21:54:23 -0300

    Init: Section

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
