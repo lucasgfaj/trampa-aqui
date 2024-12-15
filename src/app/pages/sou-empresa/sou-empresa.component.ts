c2fb788f0338fb1cc9a8c993a9c167d35435f22a
lucasgfaj <lucasgfajardo09@gmail.com>
2024-12-15 20:25:00 -0300

    Edit: sou-empresa

--- src/app/pages/sou-empresa/sou-empresa.component.ts
+++ src/app/pages/sou-empresa/sou-empresa.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-sou-empresa',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, RouterLink],
   templateUrl: './sou-empresa.component.html',
   styleUrl: './sou-empresa.component.css'
 })
