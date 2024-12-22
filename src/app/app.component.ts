8b9bdc84dfc4e54ff4dd1cce12c927581a6efe92
João Victor <jvlopes25@gmail.com>
2024-12-22 17:43:45 -0300

    areas ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardCandidatoComponent } from "./pages/dashboard-candidato/dashboard-candidato.component";
+import { DashboardEmpresaComponent } from './pages/dashboard-empresa/dashboard-empresa.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardCandidatoComponent, DashboardCandidatoComponent, DashboardEmpresaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
