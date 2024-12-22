ae595eebf6678d01d1bdddb6b7baff96a2593e92
lucasgfaj <lucasgfajardo09@gmail.com>
2024-12-21 22:17:56 -0300

    Edit: Job-Api-Test

---
+++ src/app/pages/job-api/job-api.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-api',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-api.component.html',
+  styleUrl: './job-api.component.css'
+})
+export class JobApiComponent {
+
+}
--- src/app/pages/ver-vagas/ver-vagas.component.ts
+++ src/app/pages/ver-vagas/ver-vagas.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { JobApiComponent } from '../job-api/job-api.component';
+
 @Component({
   selector: 'app-ver-vagas',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, JobApiComponent],
   templateUrl: './ver-vagas.component.html',
   styleUrl: './ver-vagas.component.css'
 })
