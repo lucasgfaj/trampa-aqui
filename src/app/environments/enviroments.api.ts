f91194340e034c331b2757b1c09864eeb62a0d75
lucasgfaj <lucasgfajardo09@gmail.com>
2024-12-22 23:12:38 -0300

    Init: API

---
+++ src/app/environments/enviroments.api.ts
@@ -0,0 +1,6 @@
+export const environmentApi = {
+  production: false,  // true no environment.prod.ts
+  apiUrl: 'https://jobs-api14.p.rapidapi.com/v2/list',
+  rapidApiKey: '9a32974eb9msh98c2b8aa92f618fp17a99ajsnd92340d62b48',
+  rapidApiHost: 'jobs-api14.p.rapidapi.com'
+};
